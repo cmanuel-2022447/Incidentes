f7c88ed00fb4a57a2b0b9702bf084c438e83a638
cmanuel-2022447 <cmanuel-2022447@kinal.edu.gt>
2026-06-12 19:42:41 -0600

    feat: se agrega incidentes.models.ts

--- src/models/incidente.model.ts
+++ src/models/incidente.model.ts
@@ -1,12 +1,18 @@
+/**
+ * Definicion de tipos restringidos (Union Types).
+ */
 export type Prioridad = 'baja' | 'media' | 'alta';
 export type EstadoIncidente = 'abierto' | 'en progreso' | 'resuelto';
 
+/**
+ * Interfaz Incidente: Representa la estructura de datos central del sistema.
+ */
 export interface Incidente {
-    readonly id: number;
-    titulo: string;
-    descripcion: string;
-    reportadoPor: string;
-    prioridad: Prioridad;
-    estado: EstadoIncidente;
-    fechaCreacion: Date;
+    readonly id: number;           // readonly: El ID no debe cambiar una vez creado
+    titulo: string;                // Titulo o asunto del reporte
+    descripcion: string;           // Detalle técnico del problema
+    reportadoPor: string;          // Nombre del usuario que genera el reporte
+    prioridad: Prioridad;          // Tipo definido: solo 'baja', 'media' o 'alta'
+    estado: EstadoIncidente;       // Tipo definido: solo 'abierto', 'en progreso' o 'resuelto'
+    fechaCreacion: Date;           // Objeto Date para el registro temporal
 }
